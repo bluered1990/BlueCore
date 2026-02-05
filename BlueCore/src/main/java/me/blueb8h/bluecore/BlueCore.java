77e598c6e7b73b81836754fc25c71778a8cbd4b6
bluered1990 <127968082+bluered1990@users.noreply.github.com>
2026-02-05 18:19:40 +0330

    Update BlueCore.java

--- BlueCore/src/main/java/me/blueb8h/bluecore/BlueCore.java
+++ BlueCore/src/main/java/me/blueb8h/bluecore/BlueCore.java
@@ -1,29 +1,53 @@
 package me.blueb8h.bluecore;
 
-import me.blueb8h.bluecore.economy.EconomyManager;
-import me.blueb8h.bluecore.rank.RankManager;
-import me.blueb8h.bluecore.tab.TabManager;
-import me.blueb8h.bluecore.teleport.TeleportCommands;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BlueCore extends JavaPlugin {
 
-    private static BlueCore instance;
+    private Location spawn;
 
     @Override
     public void onEnable() {
-        instance = this;
-        saveDefaultConfig();
-
-        new EconomyManager(this);
-        new RankManager(this);
-        new TabManager(this);
-        new TeleportCommands(this);
-
         getLogger().info("BlueCore enabled!");
     }
 
-    public static BlueCore getInstance() {
-        return instance;
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (command.getName().equalsIgnoreCase("bluecore")) {
+            sender.sendMessage("§bBlueCore §7is running!");
+            return true;
+        }
+
+        if (command.getName().equalsIgnoreCase("spawn")) {
+            if (!(sender instanceof Player player)) {
+                sender.sendMessage("Only players can use this command.");
+                return true;
+            }
+            if (spawn == null) {
+                player.sendMessage("§cSpawn is not set.");
+                return true;
+            }
+            player.teleport(spawn);
+            player.sendMessage("§aTeleported to spawn!");
+            return true;
+        }
+
+        if (command.getName().equalsIgnoreCase("setspawn")) {
+            if (!(sender instanceof Player player)) {
+                sender.sendMessage("Only players can use this command.");
+                return true;
+            }
+            spawn = player.getLocation();
+            player.sendMessage("§aSpawn set!");
+            return true;
+        }
+
+        return false;
     }
 }
